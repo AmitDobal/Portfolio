44aa14335b250dde63212f26f93365be5134c3bf
AmitDobal <2019pte5188@mnit.ac.in>
2021-09-06 22:40:36 +0530

    create portfolioList

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            genius.
+            webby.
           </a>
           <div className="itemContainer">
             <Person className="icon" />
