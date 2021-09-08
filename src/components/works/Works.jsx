35768342c69d3295a6923595ff8e64a888a184cd
AmitDobal <2019pte5188@mnit.ac.in>
2021-09-08 14:48:28 +0530

    Works component Complete

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,8 +1,10 @@
 //! Time - 1:51:00
-
+import { useState } from "react";
 import "./works.scss";
 
 function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const data = [
     {
       id: "1",
@@ -27,9 +29,18 @@ function Works() {
     },
   ];
 
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : data.length - 1)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
   return (
     <div className="works" id="works">
-      <div className="slider">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         {data.map((d) => (
           <div className="container">
             <div className="item">
@@ -50,8 +61,18 @@ function Works() {
           </div>
         ))}
       </div>
-      <img src="assets/arrow.png" className="arrow left" alt="" />
-      <img src="assets/arrow.png" className="arrow right" alt="" />
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
     </div>
   );
 }
